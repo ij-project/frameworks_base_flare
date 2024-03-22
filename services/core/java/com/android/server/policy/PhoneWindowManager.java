51f4dfb5d91f8370cd2ee8a20fbd746b6b0671fd
someone5678 <someone5678@users.noreply.github.com>
2024-03-22 12:38:38 +0900

    fixup! base: Add api to take screenshots
    * Remove partial screenshot related code from utils and Gesture Nav
    
    Change-Id: I3509b38cbc4190f935227fe4b7c99899ab385712

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -6879,9 +6879,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         String action = intent.getAction();
         if (action != null) {
             if (FlareUtils.INTENT_SCREENSHOT.equals(action)) {
-                interceptScreenshotChord(TAKE_SCREENSHOT_FULLSCREEN, SCREENSHOT_KEY_OTHER, 0);
-            } else if (FlareUtils.INTENT_REGION_SCREENSHOT.equals(action)) {
-                interceptScreenshotChord(TAKE_SCREENSHOT_SELECTED_REGION, SCREENSHOT_KEY_OTHER, 0);
+                interceptScreenshotChord(SCREENSHOT_KEY_OTHER, 0);
             }
         }
     }
